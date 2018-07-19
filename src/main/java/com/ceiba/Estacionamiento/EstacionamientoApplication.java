a5ce557f0c53f261833b86624cfc2fedf03eff51
Yuliana Cañas Cifuentes <yuliana.canas@ceiba.com.co>
2018-07-19 11:17:15 -0500

    Commit inicial proyecto Ceiba-Estacionamiento. Estructura del Proyecto

---
+++ src/main/java/com/ceiba/Estacionamiento/EstacionamientoApplication.java
@@ -0,0 +1,12 @@
+package com.ceiba.Estacionamiento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstacionamientoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstacionamientoApplication.class, args);
+	}
+}
---
+++ src/test/java/com/ceiba/Estacionamiento/EstacionamientoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.ceiba.Estacionamiento;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EstacionamientoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
